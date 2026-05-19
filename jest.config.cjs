b48ea6570ba90911df7b93d0f3fcc2586d56e088
ismael mansouri <ismael.mansouri@gmail.com>
2026-05-19 13:12:47 +0100

    FINAL

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
-    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.cjs',
-  },
-}
